0c52b0be6c3447df9a8d44dc06abd4da81c2f4c3
girinath2805 <giri28may@gmail.com>
2024-05-31 21:55:21 +0530

    Updated signin

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/flux-logo.svg",
           },
           variables: {
             colorText: "#fff",
